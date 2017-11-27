1c062c8f7c623ec01c343f6eed04341f4e12f5b3
codermckee <mckee8023@gmail.com>
2017-11-27 16:49:16 +0800

    Add files via upload

--- mapillary_downloader.py
+++ mapillary_downloader.py
@@ -4,7 +4,6 @@ import os
 import json
 from bs4 import BeautifulSoup
 import datetime
-import  MySQLdb
 datetime_format = '%Y-%m-%d %H:%M:%S'
 
 
@@ -16,13 +15,13 @@ def get_coor_list(lat_min,lat_max,lon_min,lon_max):
         while lon <lon_max :
             coordinate = (lat,lon)
             area.append(coordinate)
-            lon +=0.05
-        lat += 0.05
+            lon +=0.1
+        lat += 0.1
     return area
 
 def get_city_coor(city):
     #city = 'london'
-    handle = open('./config/city_list.txt')
+    handle = open('.\\config\\city_list.txt')
     for line in handle:
         if '\xef' in line:
             line = line.replace('\xef\xbb\xbf','')
@@ -34,7 +33,7 @@ def get_city_coor(city):
     return coor_list
 
 def get_start_date():
-    date_file = open('./config/date_config.json')
+    date_file = open('.\\config\\date_config.json')
     start_time  = date_file.read()
     start_time = json.loads(start_time)
     start_time = start_time['start_time']
@@ -63,9 +62,9 @@ def get_download_time(date,day_delta):
     download_time.append(n_day)
     return download_time
 
-def save_url(img_url,file_name):
+def save_url(img_url,lon,lat,angle,file_name):
     handle = open(file_name,'a+')
-    handle.write(img_url)
+    handle.write(img_url+' '+str(lon)+' '+str(lat)+' '+str(angle))
     handle.write('\n')
     handle.close()
 
@@ -80,16 +79,13 @@ def add_one_week(start_day,day_delta):
     refresh = {}
     refresh['start_time'] = next_day
     #print refresh
-    handle = open('./config/date_config.json','w+')
+    handle = open('.\\config\\date_config.json','w+')
     handle.write(json.dumps(refresh))
     handle.close()
 
 
 
 if __name__ == '__main__':
-    db = MySQLdb.connect('localhost','root','5#611','Mapillary')
-    cs = db.cursor()
-    #database has been connected
     city = 'london'
     last_weeks = 2
     day_delta = 7
@@ -98,12 +94,12 @@ if __name__ == '__main__':
     while last_weeks > 0:
         date = get_start_date()
         print u'==============正在抓取日期：%s ==============' % (date['start_time'].split(' ')[0])
-        datapath = './data/' + date['year'] + '/' + date['month'] + '/' + date['day']
+        datapath = '.\\data\\' + date['year'] + '\\' + date['month'] + '\\' + date['day']
         if os.path.isdir(datapath):
-            filename = datapath + '/' + city + '.txt'
+            filename = datapath + '\\' + city + '.txt'
         else:
             os.makedirs(datapath)
-            filename = datapath + '/' + city + '.txt'
+            filename = datapath + '\\' + city + '.txt'
         download_time = get_download_time(date,day_delta)
         print download_time
         start_time = download_time[0].isoformat()
@@ -114,7 +110,7 @@ if __name__ == '__main__':
             lon = str(i[1])
             lat = str(i[0])
             # print lon,type(lon),lat
-            url = 'https://a.mapillary.com/v3/images?client_id=ekcyWUdPNnkwSlRrMThjMVhWTFV0dzphYjRiMmE0MzM3YzQzMTAy&lookat=%s,%s&closeto=%s,%s&start_time=%s&end_time=%s&radius=2775'
+            url = 'https://a.mapillary.com/v3/images?client_id=ekcyWUdPNnkwSlRrMThjMVhWTFV0dzphYjRiMmE0MzM3YzQzMTAy&lookat=%s,%s&closeto=%s,%s&start_time=%s&end_time=%s&radius=10000'
             url = url % (lon, lat, lon, lat, start_time, end_time)
             print url
             res = requests.get(url)
@@ -126,13 +122,16 @@ if __name__ == '__main__':
                 for info in dic['features']:
                     key = info['properties']['key']
                     coor = info['geometry']['coordinates']
+                    angle = info['properties']['ca']
                     lon = coor[0]
                     lat = coor[1]
                     image_URL = image_URL_L + key + image_URL_R
-                    #save_url(image_URL, filename)
-                    cs.execute("insert into image values(%s,%s,%s,%s)", (key, lat, lon,image_URL))
-                    db.commit()
+                    save_url(image_URL,lon,lat,angle,filename)
                     # print image_URL+' '+str(lon)+','+str(lat)
+                    img = requests.get(image_URL)
+                    f = open('.\\data\\'+key+'.jpg','ab')
+                    f.write(img.content)
+                    f.close()
             else:
                 print '(%s,%s)----no features----' % (lat, lon)
         last_weeks -= 1
